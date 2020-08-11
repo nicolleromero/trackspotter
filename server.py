3604a6d76634baff15af8b90238a0ae113fc24fc
Nicolle Romero <nicollearomero@gmail.com>
2020-08-11 16:38:36 -0700

    Track list UI with test data

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template, jsonify, request
 # from model import db, connect_to_db, Card
 
+from pprint import pformat
+import os
+from sys import argv
+import json
+import requests
+
 app = Flask(__name__)
+app.secret_key = 'SECRETSECRETSECRET'
+
+# This configuration option makes the Flask interactive debugger
+# more useful (you should remove this line in production though)
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = True
+
+
+# API_KEY = os.environ['SPOTIFY_KEY']
 
 
 @app.route("/")
@@ -9,6 +23,39 @@ def show_homepage():
     """Show the application's homepage."""
 
     return render_template("homepage.html")
+
+
+# @app.route('/afterparty/search')
+# def find_afterparties():
+#     """Search for afterparties on Eventbrite"""
+
+#     keyword = request.args.get('keyword', '')
+#     postalcode = request.args.get('zipcode', '')
+#     radius = request.args.get('radius', '')
+#     unit = request.args.get('unit', '')
+#     sort = request.args.get('sort', '')
+
+#     parameters = [keyword, postalcode, radius, unit, sort]
+
+#     # url = 'https://app.ticketmaster.com/discovery/v2/events?'
+#     url = '	https://api.spotify.com/v1/search?'
+#     payload = {'apikey': API_KEY,
+#                'keyword': keyword,
+#                }
+
+#     # for parameter in parameters:
+#     #     if parameter:
+#     #         payload[parameter] = str(parameter)
+
+#     # res = requests.get(url, params=payload)
+#     data = res.json()
+#     events = data['_embedded']['events']
+#     print(payload)
+
+#     return render_template('search-results.html',
+#                            pformat=pformat,
+#                            data=data,
+#                            results=events)
 
 
 if __name__ == "__main__":
--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,4 +1,7 @@
+const { Component } = React;
+const { render } = ReactDOM;
 const { Provider, useSelector, useDispatch } = ReactRedux;
+const { Button, Col, Container, Form, FormControl, ListGroup, Navbar, Row, Table } = ReactBootstrap;
 
 const initialState = {
   user: '',
@@ -27,11 +30,95 @@ const initialState = {
 
 
 function App() {
+
+  const items = TEST_DATA2.tracks.items;
+
+  function millisToTime(milliseconds) {
+    var minutes = Math.floor(milliseconds / 60000);
+    var seconds = ((milliseconds % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+
   return (
     <React.Fragment>
-      <p>trackspotter Advanced Search</p>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">trackspotter
+          <img src="/static/img/spot_icon_bw.png" width="30" height="30" className="d-inline-block align-top"
+            alt="Spotify logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Some User</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Container>
+        <Row className="box">
+          <Col></Col>
+          <Col><Form inline>
+            <FormControl type="text" placeholder="Enter a search term" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>TRACK</th>
+                <th>TITLE</th>
+                <th>ARTIST</th>
+                <th>ALBUM</th>
+                <th>PLAYTIME</th>
+                <th>PREVIEW</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, i) => {
+                let order = i + 1;
+                let track_time = millisToTime(item.duration_ms);
+                let to_play = "https://open.spotify.com/embed/track/" + item.id
+
+                return (
+                  <tr>
+                    <td>{order}</td>
+                    <td>{item.name}</td>
+                    <td>{item.album.artists[0].name}</td>
+                    <td>{item.album.name}</td>
+                    <td>{track_time}</td>
+                    <td>
+                      <iframe
+                        src={to_play}
+                        width="300"
+                        height="80"
+                        frameborder="0"
+                        allowtransparency="true"
+                        allow="encrypted-media"
+                      >
+                      </iframe>
+                    </td>
+                  </tr>
+                )
+              })}
+
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <Button variant="outline-dark offset-10">Save Playlist</Button>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
